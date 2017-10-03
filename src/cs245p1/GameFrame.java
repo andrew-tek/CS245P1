658b95cf2259d4b37207532bc04187691ad6da46
Christopher Kilian <cdkilian@gmail.com>
2017-10-03 13:02:54 -0700

    Added Game Over page

--- src/cs245p1/GameFrame.java
+++ src/cs245p1/GameFrame.java
@@ -288,6 +288,7 @@ public class GameFrame extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
