32eed0f34f9b35b88474272f88e2c9cefb6fb62b
Andrew Tek <andrewtek@gmail.com>
2017-10-04 23:37:20 -0700

    Menu Panel Buttons Now Do Something

--- src/cs245p1/CS245P1.java
+++ src/cs245p1/CS245P1.java
@@ -6,12 +6,15 @@
 package cs245p1;
 
 import java.awt.Frame;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
@@ -33,13 +36,13 @@ public class CS245P1 {
         GameOverPanel gameOver = new GameOverPanel();
         
         
-        
         JFrame mainFrame = new JFrame();
         mainFrame.add(startPanel);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.pack();
         mainFrame.setVisible(true);
         mainFrame.setLocationRelativeTo(null);
+        //Sets up timer for 4000 ms... 3000ms it will change too early
         ActionListener showMenuScreen = new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
                 mainFrame.remove(startPanel);
@@ -51,5 +54,18 @@ public class CS245P1 {
         timer.setRepeats(false);
         timer.start();  
     }
+//    void static creditsButtonListener (JFrame mainFrame, JPanel creditsPanel) {
+//        creditsPanel.getCreditsButton = new JButton("auto");
+//        add(creditsPanel.creditsButton);
+//        creditsPanel.creditsButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                JFrame creditsPage = new JFrame();
+//                creditsPage.add(creditsPanel);
+//                creditsPage.setVisible(true);
+//                System.out.println("HERE");
+//            }
+//        });
+//    }
   }
     
