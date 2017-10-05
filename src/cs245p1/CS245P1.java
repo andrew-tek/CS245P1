4ed152aec35e7aa8283370ad4932515ba0484127
Andrew Tek <andrewtek@gmail.com>
2017-10-04 20:42:26 -0700

    Flows from start to function page

--- src/cs245p1/CS245P1.java
+++ src/cs245p1/CS245P1.java
@@ -5,10 +5,15 @@
  */
 package cs245p1;
 
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 /**
  *
@@ -20,27 +25,31 @@ public class CS245P1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     StartPanel startPanel = new StartPanel();
-     MenuPanel menuPanel = new MenuPanel();
-     JFrame mainFrame = new JFrame("Basic JFrame");
-     mainFrame.setSize(600, 400);
-      mainFrame.setLocationRelativeTo(null);
-     mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     mainFrame.add(startPanel);
-     mainFrame.pack();
-     mainFrame.setVisible(true);
-     
-     mainFrame.remove(startPanel);
-     mainFrame.add(menuPanel);
-//     try {
-//         System.out.println("START");
-//         TimeUnit.SECONDS.sleep(3);
-//         System.out.println("END");
-//     } catch (InterruptedException ex) {
-//         Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
-//     }
-//     mainFrame.remove(startPanel);
-//     mainFrame.add(menuPanel);
+        StartPanel startPanel = new StartPanel();
+        MenuPanel menuPanel = new MenuPanel();
+        MainGamePanel mainGamePanel = new MainGamePanel();
+        //HighScorePanel goes here
+        CreditsPanel creditPanel = new CreditsPanel();
+        GameOverPanel gameOver = new GameOverPanel();
+        
+        
+        
+        JFrame mainFrame = new JFrame();
+        mainFrame.add(startPanel);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+        mainFrame.setLocationRelativeTo(null);
+        ActionListener showMenuScreen = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                mainFrame.remove(startPanel);
+                mainFrame.add(menuPanel);
+                mainFrame.pack();
+            }
+        };
+        Timer timer = new Timer (4000, showMenuScreen);
+        timer.setRepeats(false);
+        timer.start();  
     }
+  }
     
-}
